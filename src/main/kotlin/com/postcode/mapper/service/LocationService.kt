c0e7e9c5fbfb16361988fd6bd05396b331702886
Felix Holfelder <felixholfelder1@t-online.de>
2023-07-14 22:32:57 +0200

    added mapping for locations

--- src/main/kotlin/com/postcode/mapper/service/LocationService.kt
+++ src/main/kotlin/com/postcode/mapper/service/LocationService.kt
@@ -1,7 +1,6 @@
 package com.postcode.mapper.com.postcode.mapper.service
 
 import com.google.cloud.firestore.Firestore
-import com.google.cloud.firestore.QueryDocumentSnapshot
 import com.google.firebase.cloud.FirestoreClient
 import com.postcode.mapper.com.postcode.mapper.entity.Location
 import com.postcode.mapper.com.postcode.mapper.enums.QueryParam
@@ -11,17 +10,17 @@ import org.springframework.stereotype.Service
 class LocationService {
   fun getLocations(query: String): List<Location> {
     val firestore: Firestore = FirestoreClient.getFirestore()
-    var documents: List<QueryDocumentSnapshot> = searchFor(QueryParam.POSTCODE, query, firestore)
-    if (documents.isEmpty()) {
-      documents = searchFor(QueryParam.CITY, query, firestore)
+    var locations: List<Location> = searchFor(QueryParam.POSTCODE, query, firestore)
+    if (locations.isEmpty()) {
+      locations = searchFor(QueryParam.CITY, query, firestore)
     }
-    return documents.map { it.toObject(Location::class.java) }
+    return locations
   }
 
-  fun searchFor(param: QueryParam, query: String, firestore: Firestore): List<QueryDocumentSnapshot> {
+  private inline fun <reified T> searchFor(param: QueryParam, query: String, firestore: Firestore): List<T> {
     return firestore.collection("locations")
       .whereGreaterThanOrEqualTo(param.value, query)
       .whereLessThanOrEqualTo(param.value, "${query}\uf7ff")
-      .get().get().documents
+      .get().get().toObjects(T::class.java)
   }
 }
