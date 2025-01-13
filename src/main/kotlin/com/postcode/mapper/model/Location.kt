f17009b9c4d54bb2bd545bb720bc3c022c038ecf
Felix Holfelder <felixholfelder1@t-online.de>
2025-01-13 14:34:42 +0100

    removed db

--- src/main/kotlin/com/postcode/mapper/model/Location.kt
+++ src/main/kotlin/com/postcode/mapper/model/Location.kt
@@ -3,6 +3,6 @@ package com.postcode.mapper.com.postcode.mapper.model
 data class Location(
   val postcode: String,
   val city: String,
-  val lat: String,
   val lng: String,
+  val lat: String,
 )
