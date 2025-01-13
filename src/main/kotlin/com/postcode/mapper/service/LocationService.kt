49383a75355e22c257d8d1377d043fc10ce7f041
Felix Holfelder <felixholfelder1@t-online.de>
2025-01-13 11:13:20 +0100

    improved sorting for locations

--- src/main/kotlin/com/postcode/mapper/service/LocationService.kt
+++ src/main/kotlin/com/postcode/mapper/service/LocationService.kt
@@ -10,19 +10,23 @@ import org.springframework.stereotype.Service
 
 @Service
 class LocationService(val mongoOperations: MongoOperations) {
-  val CASE_INSENSITIVE = "i";
+  val CASE_INSENSITIVE = "i"
   fun getLocations(queryString: String): List<Location> {
     var query = Query().addCriteria(Criteria.where("postcode").regex(toLikeRegex(queryString), CASE_INSENSITIVE))
     var locationEntities: List<LocationEntity> = mongoOperations.find(query, LocationEntity::class.java).sortedBy { it.postcode }
     if (locationEntities.isEmpty()) {
       query = Query().addCriteria(Criteria.where("city").regex(toLikeRegex(queryString), CASE_INSENSITIVE))
       locationEntities = mongoOperations.find(query, LocationEntity::class.java)
-        .sortedWith(compareBy(LocationEntity::city, LocationEntity::postcode))
+        .sortedWith { a, b ->
+          when {
+            a.city == queryString -> -1   // "a" matches the search key exactly, so it comes first
+            b.city == queryString -> 1    // "b" matches the search key exactly, so it comes after "a"
+            else -> a.city!!.compareTo(b.city!!)  // If neither matches exactly, sort alphabetically
+          }
+        }
     }
     return locationEntities.map { it.toModel() }.toList()
   }
 
-  private fun toLikeRegex(source: String): String {
-    return source.replace("\\*".toRegex(), ".*")
-  }
+  private fun toLikeRegex(source: String): String = source.replace("\\*".toRegex(), ".*")
 }
